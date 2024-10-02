e0b7924b922ac64581e9705f9fe0309d1ace4fc0
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2024-10-02 23:39:12 +0530

    Create CodingScoreCheck.js

---
+++ Asynchronous JS/CodingScoreCheck.js
@@ -0,0 +1,14 @@
+function codingScoreCheck(marks, cutoff) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const total = marks.reduce((acc, score) => acc + score, 0);
+            const average = total / marks.length;
+
+            if (average >= cutoff) {
+                resolve(average);
+            } else {
+                reject("Sorry, you haven't cleared the coding round.");
+            }
+        }, 2000);
+    });
+}
